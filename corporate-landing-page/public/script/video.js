3c6a0b5143133c1418221c1b4bdf53766a917ded
Timur <dkatimullin@gmail.com>
2019-10-15 21:44:03 +0400

    Implements 3rd mock up.

---
+++ corporate-landing-page/public/script/video.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  const playLogoPath = '../public/images/play-icon.svg';
+  const pauseLogoPath = '../public/images/pause-icon.svg';
+  let playButton = document.querySelector('.play-container');
+  let buttonLogo = document.querySelector('.play-container__logo');
+  const leftMargin = buttonLogo.style.marginLeft;
+  let video = document.querySelector('.video-bg');
+  playButton.onclick = function(event) {
+    if (video.paused) {
+      video.play();
+      buttonLogo.src = pauseLogoPath;
+      buttonLogo.style.marginLeft = 0;
+    } else {
+      video.pause();
+      buttonLogo.src = playLogoPath;
+      buttonLogo.style.marginLeft = leftMargin;
+    }
+  };
+});
